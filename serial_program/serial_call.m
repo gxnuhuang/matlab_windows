baab915e66d308a9142075e6c050cafaea9ed06a
gxnuhuang <343862618@qq.com>
2014-09-10 11:21:58 +0800

    linux串口与Matlab通信，linux发送，Matlab接收

--- serial_program/serial_call.m
+++ serial_program/serial_call.m
@@ -1,9 +1,13 @@
 clc;
 s = serial('COM1');     % creat serial port obj
-set(s,'BaudRate',115200,'DataBits',8,'StopBits',1,...
+set(s,'BaudRate',38400,'DataBits',8,'StopBits',1,...
     'Parity','none','FlowControl','none');  % set properties for serial
 
+
 s.BytesAvailableFcnMode = 'terminator'; % byte number or terminator
+
+% s.BytesAvailableFcnCount = 40;
+% s.BytesAvailableFcnMode = 'byte';
 s.BytesAvailableFcn = {@serial_callback};   % {@mycallback,time}
 
 fopen(s);
