347de73843caf6f962e10c90e7810e0b16614810
gxnuhuang <343862618@qq.com>
2014-09-10 17:49:03 +0800

    linux串口接收，Matlab串口发送

--- serial_program/serial_call.m
+++ serial_program/serial_call.m
@@ -1,6 +1,6 @@
 clc;
 s = serial('COM1');     % creat serial port obj
-set(s,'BaudRate',38400,'DataBits',8,'StopBits',1,...
+set(s,'BaudRate',115200,'DataBits',8,'StopBits',1,...
     'Parity','none','FlowControl','none');  % set properties for serial
 
 
@@ -9,10 +9,14 @@ s.BytesAvailableFcnMode = 'terminator'; % byte number or terminator
 % s.BytesAvailableFcnCount = 40;
 % s.BytesAvailableFcnMode = 'byte';
 s.BytesAvailableFcn = {@serial_callback};   % {@mycallback,time}
-
+i=0;
 fopen(s);
-
-pause;      %right here is receiving the data and plot
+while(1)
+i=i+1;
+fprintf(s,'zq:987:123:459matlab%fh',i)
+end
+%fprintf('hzq:matlab send test\r\n')
+%pause;      %right here is receiving the data and plot
 fclose(s);
 delete(s)
 clear s
